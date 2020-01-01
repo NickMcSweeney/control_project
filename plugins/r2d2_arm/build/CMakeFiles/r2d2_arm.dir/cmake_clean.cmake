07f5c35595bd1ac4f7adc0ea47e97fd049dbff1e
Nicholas Shindler <shindler25@gmail.com>
2020-01-01 14:16:30 +0100

    just a bit of a cluster

---
+++ plugins/r2d2_arm/build/CMakeFiles/r2d2_arm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/r2d2_arm.dir/r2d2_arm_plugin.cc.o"
+  "libr2d2_arm.pdb"
+  "libr2d2_arm.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/r2d2_arm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
